de1c36bba307a6b91e4bb2b93ee0efb339893c86
Tabipo <danielebarcelon3@gmail.com>
2025-11-27 00:55:20 +0800

    refactor(menu table seeder): changed menuData content
    - removed temporary menu items
    - added new menu items to MenuSeeder

--- backend/app/Database/Seeds/MenuSeeder.php
+++ backend/app/Database/Seeds/MenuSeeder.php
@@ -12,21 +12,46 @@ class MenuSeeder extends Seeder
 
         $menuData = [
             [
-                'name' => 'Americano',
-                'description' => 'Espresso softened with hot water for a bold and classic taste.',
-                'price' => '65',
+                'name' => 'Pepperoni Pizza',
+                'description' => 'A simple yet satisfying classic with rich tomato sauce and a blend of perfectly melted cheeses on a golden, crispy crust.',
+                'price' => '355',
                 'is_available' => '1',
                 'createdAt' => $now,
                 'updatedAt' => $now,
             ],
             [
-                'name' => 'Latte',
-                'description' => 'A balance of espresso, steamed milk, and topped with a layer of milk foam',
-                'price' => '80',
+                'name' => 'Cheese Pizza',
+                'description' => 'A classic favorite topped with tomato sauce, melted mozzarella, and generous slices of smoky pepperoni on a perfectly baked crust.',
+                'price' => '295',
+                'is_available' => '1',
+                'createdAt' => $now,
+                'updatedAt' => $now,
+            ],
+            [
+                'name' => 'Hawaiian Pizza',
+                'description' => 'A sweet and savory combination of juicy pineapple, smoky ham, and melted mozzarella over a rich tomato base.',
+                'price' => '335',
+                'is_available' => '1',
+                'createdAt' => $now,
+                'updatedAt' => $now,
+            ],
+            [
+                'name' => 'Veggie Supreme Pizza',
+                'description' => 'A colorful mix of bell peppers, onions, mushrooms, black olives, and tomatoes atop a mozzarella-covered, sauce-brushed crust.',
+                'price' => '375',
+                'is_available' => '1',
+                'createdAt' => $now,
+                'updatedAt' => $now,
+            ],
+            [
+                'name' => "Meat Lover's Pizza",
+                'description' => 'Loaded with pepperoni, ham, sausage, and bacon, layered over mozzarella and savory tomato sauce for a hearty bite.',
+                'price' => '375',
                 'is_available' => '1',
                 'createdAt' => $now,
                 'updatedAt' => $now,
             ]
+
         ];
 
         $this->db->table('Menu')->insertBatch($menuData);
