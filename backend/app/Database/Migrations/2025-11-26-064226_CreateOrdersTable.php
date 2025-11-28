8f754ad5b6fce9de3a1defb5b62ac95aa39b51ba
Tabipo <danielebarcelon3@gmail.com>
2025-11-29 02:24:55 +0800

    refactor(orders table): added more fields
    - added order_number field
    - added quantity field
    - added first_name field
    - added last_name field
    - updated status field
    - added new foreign key

--- backend/app/Database/Migrations/2025-11-26-064226_CreateOrdersTable.php
+++ backend/app/Database/Migrations/2025-11-26-064226_CreateOrdersTable.php
@@ -16,9 +16,25 @@ class CreateOrdersTable extends Migration
                 'auto_increment' => true,   // optional if you want auto counting, but important for the id
                 'null'           => false,  // needed for most, it means it can be empty
             ],
+            'order_number' => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 50,
+                'unique'         => true,
+                'null'           => true,
+            ],
             'user_id' => [
                 'type'           => 'INT',
-                'constraint'     => 11,
+                'unsigned'       => true,
+                'null'           => false,
+            ],
+            'menu_id' => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'null'           => false,
+            ],
+            'quantity' => [
+                'type'           => 'INT',
+                'default'        => 1,
                 'null'           => false,
             ],
             'total_price' => [
@@ -27,10 +43,20 @@ class CreateOrdersTable extends Migration
                 'unsigned'       => true,
                 'null'           => false,
             ],
-            'status' => [
+            'first_name' => [
                 'type'           => 'VARCHAR',
                 'constraint'     => '100',
-                'null'           => false,
+                'null'           => true,
+            ],
+            'last_name' => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '100',
+                'null'           => true,
+            ],
+            'status' => [
+                'type'           => 'ENUM',
+                'constraint'     => ['Pending', 'Completed', 'Cancelled'],
+                'default'        => 'Pending',
             ],
             'deletedAt' => [
                 'type' => 'DATETIME',
@@ -49,6 +75,8 @@ class CreateOrdersTable extends Migration
         $this->forge->addKey('id', true);
 
         $this->forge->addForeignKey('user_id', 'Users', 'id', 'CASCADE', 'CASCADE');
+
+        $this->forge->addForeignKey('menu_id', 'Menu', 'id', 'CASCADE', 'CASCADE');
 
         $this->forge->createTable('Orders', true);
     }
