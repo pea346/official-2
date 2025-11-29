5e18ad66b180885c412fea435fd3480e583d9779
HelloJirou <jirojayandrei1111@gmail.com>
2025-11-29 17:26:06 +0800

    feat(item): add Item entity and ItemsModel with validation rules

---
+++ Entities/Item.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entities;
+
+use CodeIgniter\Entity\Entity;
+
+class Item extends Entity
+{
+    protected $datamap = [];
+    protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
+    protected $casts   = [
+        'is_available' => 'boolean',
+        'is_active' => 'boolean',
+    ];
+}
